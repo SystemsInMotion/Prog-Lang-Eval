5ece24649d59cf29590c706ccbb675066f86f31d
Drew Benson <abenson@systemsinmotion.com>
2013-11-14 14:29:59 -0500

    Adding db integration to candidate_test

--- application/models/candidate_test.php
+++ application/models/candidate_test.php
@@ -4,6 +4,10 @@ class Candidate_test extends CI_Model {
 	
 	private $table = 'candidate_tests';
 	
+	private $id;
+	
+	private $completed;
+	
 	function __construct() {
 		parent::__construct();
 
@@ -26,10 +30,19 @@ class Candidate_test extends CI_Model {
     	$this->load->model('Candidate');
     	$loaded = $this->Candidate->findById($row->candidate_id);
 
-//    	$this->load->model('Test');
-//    	$loaded &= $this->Test->findById($row->test_id);
+    	$this->load->model('Available_test');
+    	$loaded &= $this->Available_test->findById($row->test_id);
+    	
+    	$this->id = $row->id;
+    	$this->completed = $row->completed;
 
 		return $loaded;
+    }
+    
+    public function saveAnswers($answers) {
+    	$this->load->model('Candidate_answers');
+    	
+    	$this->Candidate_answers->saveAnswers($this->id, $answers);
     }
     
     public function getCandidate() {
@@ -37,7 +50,27 @@ class Candidate_test extends CI_Model {
     }
     
     public function getTest() {
-    	return $this->Test;
+    	return $this->Available_test;
+    }
+    
+    public function getId() {
+    	return $this->id;
+    }
+    
+    public function recordStart() {
+    	$this->db->query('UPDATE '.$this->table.' SET `started`=NOW() WHERE `id`='.$this->id.';');
+    }
+    
+    public function recordComplete() {
+    	$this->db->query('UPDATE '.$this->table.' SET `completed`=NOW() WHERE `id`='.$this->id.';');
+    }
+    
+    public function isCompleted() {
+    	return $this->completed != null;	
+    }
+    
+    public function recordScore($score) {
+    	$this->db->update($this->table, array('final_score' => $score), array('id' => $this->id));
     }
 
 }
