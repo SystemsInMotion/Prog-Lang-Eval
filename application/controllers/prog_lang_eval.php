5a33e7e35828712467332af99e182774fd2196e3
Drew Benson <abenson@systemsinmotion.com>
2013-11-14 14:29:42 -0500

    Still refactoring the controller

--- application/controllers/prog_lang_eval.php
+++ application/controllers/prog_lang_eval.php
@@ -12,19 +12,18 @@ class Prog_lang_eval extends CI_Controller {
 	private function _prepare_test(Candidate_test $ctest, $review_mode) {
 		$file = $ctest->getTest()->getFilename();
 		
-		$xml = simplexml_load_file('assets/xml/tests/'.$file);
+		$shuffle = ! $review_mode;
 		
-		$this->load->library('Test_parser');
-		
-		$test = $this->test_parser->parse($xml);
+		$test = $this->_parseTest($file);
 		
 		$data['candidate'] = $ctest->getCandidate()->getName();
+		
 		$data['test_name'] = $test->getName();
-		
+		$data['version'] = $test->getId();
 		$data['intro'] = $test->getIntro();
-		$data['questions'] = $test->getQuestions(true);
+		$data['questions'] = $test->getQuestions($shuffle);
 		
-		$data['shuffle'] = ! $review_mode;
+		$data['shuffle'] = $shuffle;
 		$data['review'] = $review_mode;		
 		
 		return $data;
@@ -38,14 +37,16 @@ class Prog_lang_eval extends CI_Controller {
 		
 		$found = $this->Candidate_test->findByCode($code);
 		
-		if (! $found) {
+		if (! $found || $this->Candidate_test->isCompleted()) {
 			$data['error'] = true;
 			$this->load->view('start_view', $data);
 		}
 		else {
 			$this->session->set_userdata(array('code' => $code));
 			
-			$data = $this->_prepare_test($this->Candidate_test, false);
+			$data = $this->_prepare_test($this->Candidate_test, true);
+			
+			$this->Candidate_test->recordStart();
 
 			$this->load->view('prog_lang_eval_view', $data);
 		}
@@ -58,25 +59,53 @@ class Prog_lang_eval extends CI_Controller {
 	}
 
 	public function submit() {
-		
-		$this->load->library('Test_parser');
-		
-		$xml = simplexml_load_file('assets/xml/tests/java_2.1.xml');
-		
-		$test = $this->test_parser->parse($xml);
-		
+		$code = $this->session->userdata('code');
 		$answers = $this->input->post();
+		
+		$this->load->model('Candidate_test');
+		$found = $this->Candidate_test->findByCode($code);
+		
+		if ($found) {
+			$saved = $this->Candidate_test->saveAnswers($answers);
+		}
+		
+		if ( ! ($found && $saved)) {
+			//Alert the user and dump the answer data to the page to be recovered by the proctor
+		}
+		
+		$this->Candidate_test->recordComplete();
+		
+		$testFileName = $this->Candidate_test->getTest()->getFilename();
+		$test = $this->_parseTest($testFileName);
 		
 		$this->load->library('Test_scorer');
 		
 		$this->test_scorer->score($test, $answers);
 		
+		$this->Candidate_test->recordScore($test->getTotalScore());
+		
+		$data['candidate'] = $this->Candidate_test->getCandidate()->getName();
+		$data['error'] = false;
+		
+		$this->load->view('test_complete_view', $data);
+	}
+	
+	public function review() {
 		$distribution = $test->getScoreDistribution(Test_scorer::getAllScores());
 		$data['graph_data'] = $this->_encodeForBarGraph($distribution);
 		$data['test'] = $test;
 		
 		$this->load->view('test_results_view', $data);
-
+	}
+	
+	private function _parseTest($testFileName) {
+		$this->load->library('Test_parser');
+		
+		$xml = simplexml_load_file('assets/xml/tests/'.$testFileName);
+		
+		$test = $this->test_parser->parse($xml);
+		
+		return $test;
 	}
 	
 	private function _encodeForBarGraph($levels) {
