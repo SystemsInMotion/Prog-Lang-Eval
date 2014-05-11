cbb07ba16380b0a65f7ca2000aa511c3a4d15143
Andrew D Benson <drew@adbenson.net>
2014-05-11 16:24:27 -0400

    Basically starting over.

--- backend/application/config/config.php
+++ backend/application/config/config.php
@@ -224,7 +224,7 @@ $config['cache_path'] = '';
 | MUST set an encryption key.  See the user guide for info.
 |
 */
-$config['encryption_key'] = '';
+$config['encryption_key'] = 'a305t9nab23t0n9a23n98p7afw8';
 
 /*
 |--------------------------------------------------------------------------
